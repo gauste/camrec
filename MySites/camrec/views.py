134412863e8200b80bfe8182f033c50a1f51751c
cookievan <vantrinh81@gmail.com>
2013-04-30 05:07:20 -0500

    Add price to returned camera list.

--- MySites/camrec/views.py
+++ MySites/camrec/views.py
@@ -40,49 +40,47 @@ def index(request):
 def category(request, cat):
 	q = QueryDict(cat)
 	f = open('cameras', 'r')
-        camera_info = pickle.load(f)
-        f.close()
+	camera_info = pickle.load(f)
+	f.close()
 
-        all_zero = True
-        if len(q) != 1:
-                for category in q:
-                        try:
-                                weight = int(q[category])
-                                if weight != 0:
-                                        all_zero = False
-                        except:
-                                continue
-        else:
-                all_zero = False
-                category_weights = {q.keys()[0]: 1}
+	all_zero = True
+	if len(q) != 1:
+			for category in q:
+					try:
+							weight = int(q[category])
+							if weight != 0:
+									all_zero = False
+					except:
+							continue
+	else:
+			all_zero = False
+			category_weights = {q.keys()[0]: 1}
 
-        if all_zero:
-                return HttpResponseRedirect('/camrec/')
+	if all_zero:
+			return HttpResponseRedirect('/camrec/')
 
-        if len(q) > 1:
-                category_weights = {category: float(q[category]) for category in q}
+	if len(q) > 1:
+			category_weights = {category: float(q[category]) for category in q}
 
-        top_cameras = get_top_cameras(n=100)
-        top_cameras = weighted_camera_scores(top_cameras, **category_weights)
-	cameras = [{'camera':'Powershot G3', 'price': '$300', 'photos':['http://farm2.static.flickr.com/1103/567229075_2cf8456f01_m.jpg', 'http://farm2.static.flickr.com/1103/567229075_2cf8456f01_m.jpg', 'http://farm2.static.flickr.com/1103/567229075_2cf8456f01_m.jpg']},
-			   {'camera':'Nikon D600', 'price': '$1200', 'photos':['http://farm2.static.flickr.com/1103/567229075_2cf8456f01_m.jpg']},
-			   {'camera':'Canon Mark 5D', 'price': '$2500', 'photos':['http://farm2.static.flickr.com/1103/567229075_2cf8456f01_m.jpg']},
-			   {'camera':'Olympus Evolt E-330', 'price': '$200', 'photos':['http://farm2.static.flickr.com/1103/567229075_2cf8456f01_m.jpg']},
-			   {'camera':'Nikon D90', 'price': '$800', 'photos':''},
-				]
+	top_cameras = get_top_cameras(n=15)
+	top_cameras = weighted_camera_scores(top_cameras, **category_weights)
+	top_camera_prices = []
+	for i in range(len(top_cameras)):
+		camera_name = top_cameras[i][0]
+		top_camera_prices.append(camera_info[camera_name]['price'])
+	
+	cameras = [{'camera': top_cameras[i][0], 'price': '%s' % (top_camera_prices[i]), 'photos':[]} for i in range(len(top_cameras))]
 
-        cameras = [{'camera': top_cameras[i][0], 'price': '%s' % (top_cameras[i][1]), 'photos':[]} for i in range(len(top_cameras))]
+	category_photo_data = load_category_photo_data()
+	day_stats = analyze_photos(category_photo_data, time_of_day = "day", **category_weights)
+	day_apertureData, day_apertureTicks = get_aperture_plot_data(day_stats)
+	day_exposureData, day_exposureTicks = get_exposure_plot_data(day_stats)
+	day_focalLengthData, day_focalLengthTicks = get_focal_length_plot_data(day_stats)
 
-        category_photo_data = load_category_photo_data()
-        day_stats = analyze_photos(category_photo_data, time_of_day = "day", **category_weights)
-        day_apertureData, day_apertureTicks = get_aperture_plot_data(day_stats)
-        day_exposureData, day_exposureTicks = get_exposure_plot_data(day_stats)
-        day_focalLengthData, day_focalLengthTicks = get_focal_length_plot_data(day_stats)
-
-        night_stats = analyze_photos(category_photo_data, time_of_day = "night", **category_weights)
-        night_apertureData, night_apertureTicks = get_aperture_plot_data(night_stats)
-        night_exposureData, night_exposureTicks = get_exposure_plot_data(night_stats)
-        night_focalLengthData, night_focalLengthTicks = get_focal_length_plot_data(night_stats)
+	night_stats = analyze_photos(category_photo_data, time_of_day = "night", **category_weights)
+	night_apertureData, night_apertureTicks = get_aperture_plot_data(night_stats)
+	night_exposureData, night_exposureTicks = get_exposure_plot_data(night_stats)
+	night_focalLengthData, night_focalLengthTicks = get_focal_length_plot_data(night_stats)
 
 	#apertureData = [35, 20, 145, 51, 151, 88, 99, 185, 75, 43];
 	#apertureTicks = ['0.0 - 1.8', '2.0 - 2.5', '2.6 - 3.2', '3.3 - 3.8', '3.9 - 4.3', '4.5 - 5.0', '5.1 - 5.9', '6.3 - 9.0', '9.5 - 14.0', '16.0 - 38.0'];
